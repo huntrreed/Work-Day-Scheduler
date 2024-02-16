e627cafd400bf1dd7471e7e86b87fe79d7c34ceb
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-02-16 10:36:52 -0700

    fixed the local storage issue, things not saving

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Work-Day-Scheduler
-
+## Description
 It can be a full time job keeping track of all the meetings and appointments you have set throughout the work day. A simple solution to this is to have a web app that  you can enter tasks and meetings throughout the day to keep yourself on schedule. 
 
 In this web app, you can see an outline of the work day from 9-5 with time blocks for each hour. For each time block you can click in the block to add an appointment, meeting, or task that you need to complete at a certian time. Since these events are stored to local storage, they will remain on your scheduler even if you click out of the app or close the page and pull it back up later. 
 
 In order to help keep track of time throughout the day, there is also a built in feature that shows the hours that have already passed in grey, the current hour in red, and future hours of the day in green. Since it can be easy to get carried away in tasks, this feature will help keep you on track to complete everything you need to complete throughout the day! 
 
+## Table of Contents
+ - [Usage](#usage)
+ - [Demo](#demo)
+ - [Questions](#questions)
+ - [Deployed Link](#deployed-link)
+
+## Usage
+This application can be used via the link on any web browser. No extra extensions are needed and the data the user enters is stored to local storage on their own device. 
+
+## Demo
 Here is a following example of what the scheduler looks like. 
 
 ![Screenshot of the Workday Scheduler App.](./Assets/Images/Screenshot%202023-11-20%20at%205.23.35 PM.png)
@@ -15,5 +25,13 @@ And if you want to add an event to a certian hour -- here is an example of how t
 
 ![Screenshot of the Workday Scheduler App when clicking into an hour box to schedule a task](./Assets/Images/Screenshot%202023-11-20%20at%205.24.03 PM.png)
 
+## Questions
 
+Contact me with any other questions regarding this project via the email listed below
+
+GitHub: [@huntrreed](https://github.com/huntrreed)
+
+Email: huntrreed@gmail.com
+
+### Deployed Link
 The deployed web app can be found at the following link: https://huntrreed.github.io/Work-Day-Scheduler/
--- script.js
+++ script.js
@@ -1,93 +1,77 @@
+// Function to display the current date in the header of the page once the page is opened. (Looked up the formatting of the Date pull from the day.js file on how to display current day)
+$(function () {
+  function displayCurrentDay() {
+    var currentDay = dayjs().format('dddd, MMMM D, YYYY');
+    $('#currentDay').text(currentDay);
+  }
+  // Callback for the function to display the current day
+  displayCurrentDay();
+});
+// Function to create time blocks with each hour on the calendar as a variable
+function createTimeBlocks() {
+  var workHours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
-  // Function to display the current date in the header of the page once the page is opened. (Looked up the formatting of the Date pull from the day.js file on how to display current day)
-  $(function () {
-    function displayCurrentDay() {
-      var currentDay = dayjs().format('dddd, MMMM D, YYYY');
-      $('#currentDay').text(currentDay);
-    }
-    // Callback for the function to display the current day
-    displayCurrentDay();
-  });
- // Function to create time blocks with each hour on the calendar as a variable
-  $(function () {
-    function createTimeBlocks() {
-      var workHours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+  workHours.forEach(function(hour, index) {
+    // variables for each hour to add a time row, an column with the hour listed, a spot to write events, and a save button
+    var timeBlock = $('<div>').addClass('row time-block').attr('id', 'hour-' + (index + 9));
+    var hourCol = $('<div>').addClass('col-1 hour text-center py-3').text(hour);
+    var textArea = $('<textarea>').addClass('col-8 col-md-10 description');
+    var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
+    saveBtn.append($('<i>').addClass('fas fa-save'));
 
-
-
-      workHours.forEach(function(hour, index) {
-        // variables for each hour to add a time row, an column with the hour listed, a spot to write events, and a save button
-        var timeBlock = $('<div>').addClass('row time-block').attr('id', 'hour-' + (index + 9));
-        var hourCol = $('<div>').addClass('col-1 hour text-center py-3').text(hour);
-        var textArea = $('<textarea>').addClass('col-8 col-md-10 description');
-        var saveBtn = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
-        saveBtn.append($('<i>').addClass('fas fa-save'));
-  
-        // Attaching the hours, text inputs, and save buttons to the time block from line 11
-        timeBlock.append(hourCol, textArea, saveBtn);
-  
-        // Appending the time block to the outer container that the whole calendar is in
-        $('.container-lg').append(timeBlock);
-      });
-    }
-    createTimeBlocks();
-
-  });
-//Updating time blocks to different classes: grey for the past, red for the current hour, and green for the future 
-  $(function () {
-    function updateTimeBlockClasses() {
-      // Setting variable for the current time from the day.js file
-      var currentHour = dayjs().hour();
-  
-      // Cycle through each time block seperately so they can all be updated individually - the id attribute selects the number instead of the hour label and the parse to make the variables numbers instead of strings so they can work with the day.js file
-      $('.time-block').each(function () {
-        var blockHour = parseInt($(this).attr('id').split('-')[1]);
-  
-        // Remove the previous class from the time block so that it can be updated with new class based on current time (not sure if this is needed but it was not updating on refresh without)
-        $(this).removeClass('past present future');
-        $(this).find('.current-hour-text').remove();
-  
-        // Apply new class based on current time (using 'this' to refer to each seperate time block)
-        if (blockHour < currentHour) {
-          $(this).addClass('past');
-        } else if (blockHour === currentHour) {
-          $(this).addClass('present');
-        } else {
-          $(this).addClass('future');
-        }
-      });
-    }
-    // Callback to update classes
-    updateTimeBlockClasses();
-  });
-  //Updating time blocks with events and saving those events to local storage so they can be reloaded later
-  (function() {
-    function saveEvent(hour, eventText) {
-      localStorage.setItem('event- ', eventText);
-    }
-  
-    // Click event on the save button will save the event info entered into the box
-    $('.saveBtn').click(function () {
-      var hour = $(this).closest('.time-block').attr('id').split('-')[1];
-      var eventText = $(this).siblings('.description').val();
-  
-      // Callback to save the event
-      saveEvent(hour, eventText);
+      // Attaching the hours, text, and save buttons to the time block from line 11
+      timeBlock.append(hourCol, textArea, saveBtn);
+      $('.container-lg').append(timeBlock);
     });
+  }
+
+//createTimeBlocks and displaySavedEvents when page is loaded
+$(document).ready(function() {
+  createTimeBlocks();
+  displaySavedEvents(); 
+});
+
+//Updating time blocks to different classes: grey for the past, red for the current hour, and green for the future 
+function updateTimeBlockClasses() {
+  var currentHour = dayjs().hour();
+  $('.time-block').each(function () {
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
+    $(this).removeClass('past present future');
+    
+    if (blockHour < currentHour) {
+      $(this).addClass('past');
+    } else if (blockHour === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
+}
+    // Callback to update classes and colors every minute 
+$(document).ready(function() {
+  updateTimeBlockClasses();
+  setInterval(updateTimeBlockClasses, 60000); 
+  }); 
+
+//event listener for saving events 
+$(document).on('click', '.saveBtn', function () {
+  var hour = $(this).closest('.time-block').attr('id').split('-')[1];
+  var eventText = $(this).siblings('.description').val();
+  
+  saveEvent(hour, eventText);
+});
   
 //Function for when you come back to the page after closing, the events will be pulled from local storage and still show on the calendar
-    function displaySavedEvents() {
-      $('.time-block').each(function() {
-        var hour = $(this).attr('id').split('-')[1];
-        var savedEvent = localStorage.getItem('event-', hour);
-        if (savedEvent) {
-          $(this).find('.description').val(savedEvent);
-        }
-      });
+function saveEvent(hour, eventText) {
+  localStorage.setItem('event-' + hour, eventText);
+}
+
+function displaySavedEvents() {
+  $('.time-block').each(function() {
+    var hour = $(this).attr('id').split('-')[1];
+    var savedEvent = localStorage.getItem('event-' + hour);
+    if (savedEvent) {
+      $(this).find('.description').val(savedEvent);
     }
-    // Callback to load any saved events
-        displaySavedEvents();
   });
-  
-
-
+}
